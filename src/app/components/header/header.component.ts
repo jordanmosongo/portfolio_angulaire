dc936e884a9c4e2bdb23220881f2d6aac642ae77
jordanmosongo <mosongojordan@gmail.com>
2021-08-01 15:22:13 +0200

    feat : add header architecture

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -8,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   constructor() { }
+  showMenu  = false;
+  handleMenuClick(){
+    this.showMenu = !this.showMenu;
+  }
 
   ngOnInit(): void {
   }
